69eecec650e65de77628f24a28de17ea84091ca3
BALJ55 <byronamh@gmail.com>
2017-11-21 15:24:46 -0600

    proyecto terminado

--- Particiones.java
+++ Particiones.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 //Ana lucia Diaz Leppe 151378
+//Byron Andrés Mota Hernández 15246		
 public class Particiones
 {
     public static void print(int[]p, int n)
@@ -10,21 +11,29 @@ public class Particiones
     }
     
     // prepara recursiva
-    public static void partition(int n) {
-        partition(n, n, "");
+    public static void partition(int n, int len) {
+        partition(n, n, "",len);
     }
+	public static int strangle(String archive){
+		String temp = archive.replace(" ", "");
+		return temp.length();
+	}
     // si la n determinada anteriormente es diferente a 0. se imprima la sumas acumulativas.
     //En las sumas acumulativas se determino n-i. En donde a la n ingresada le voy restando i. 
-    public static void partition(int n, int max, String accumulated_sums) {
+    public static void partition(int n, int max, String accumulated_sums, int max_length) {
         // 
-        if (n == 0) {
-            System.out.println(accumulated_sums);
+        if (n==0) {
+			if(max_length==0){
+				System.out.println(accumulated_sums);
+			}else if(strangle(accumulated_sums)==max_length){
+					System.out.println(accumulated_sums);
+			}
             return;
         }
         // recursive
         int i;
         for (i = Math.min(max, n); i >= 1; i--) {
-            partition(n-i, i, accumulated_sums + " " + i);
+            partition(n-i, i, accumulated_sums + " " + i,max_length);
         }
     }
     //codigo para generar la particion se realizo con un while con el objetivo que se va acumulando 
@@ -64,7 +73,9 @@ public class Particiones
         System.out.println("Ingresa el numero entero:");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        partition(n);
+		System.out.println("Ingrese la longitud de la partición");
+		int len=sc.nextInt();
+        partition(n,len);
         sc.close();
     }
 }
